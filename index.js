041075c23bb6980d844f75024d35c33302cfa80e
dhazlettjr <d.hazlett.jr@gmail.com>
2018-03-01 14:36:50 -0600

    created a basic CLI to find a list of words from a dictionary that contain the input word

---
+++ index.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+
+//SEARCH FOR WORDS ON A MACHINE DICTIONARY
+
+const userInput = process.argv[2] ? process.argv[2].toLowerCase():null;
+
+const {createReadStream} = require('fs');
+const {Writable} = require('stream');
+const {map, split} = require('event-stream');
+// import our transform stream
+const limitToTen = require('./transforms/limit_to_ten');
+
+const writeStream = Writable();
+const wordListStream = createReadStream('/usr/share/dict/words');
+
+writeStream._write = (word, _, next) => {
+    if (word.toString() === "limit reached"){
+        console.log('limit reached');
+        process.exit();
+    }
+    process.stdout.write(word);
+    next();
+};
+
+if (!userInput) {
+    console.log('Usage: readfile [search term]');
+    process.exit();
+}
+
+wordListStream
+.pipe(split())
+.pipe(map((word, next) => {
+    word.toString().toLowerCase().includes(userInput) ? next(null, word + "\n") : next();
+}))
+.pipe(limitToTen())
+.pipe(writeStream);
